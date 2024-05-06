7b3146086f2f7af28b54b8c8bd8381a51dc732ba
Amit Patyal <amitpatyalgmail.com>
2024-05-06 14:43:29 +0530

    initial update

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center mt-5">
+      <Button>Hello</Button>
+    </div>
+  );
+};
+
+export default App;
